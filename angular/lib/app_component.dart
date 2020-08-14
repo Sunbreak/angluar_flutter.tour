ef77e66756f27979808636c0c97dc73ecdf55186
Sunbreak <sunbreak.wang@gmail.com>
2020-08-14 14:17:04 +0800

    Impl angulardart project
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- angular/lib/app_component.dart
+++ angular/lib/app_component.dart
@@ -1,16 +1,49 @@
 import 'package:angular/angular.dart';
+import 'package:core/list_view_model.dart';
+import 'package:core/mapper.dart';
+import 'package:core/model/movie.dart';
+import 'package:core/movie_mapper.dart';
+import 'package:core/service.dart';
+import 'package:http/http.dart' as http;
 
-import 'src/todo_list/todo_list_component.dart';
-
-// AngularDart info: https://angulardart.dev
-// Components info: https://angulardart.dev/components
+import 'src/movie/movie_module.dart';
 
 @Component(
   selector: 'my-app',
-  styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [TodoListComponent],
+  directives: [coreDirectives],
+  providers: [
+    ClassProvider(http.Client),
+    ClassProvider(Mapper, useClass: MovieMapper),
+    ValueProvider.forToken(key, "b445ca0b"),
+    ValueProvider.forToken(host, "www.omdbapi.com"),
+    FactoryProvider(Service, provideService),
+    FactoryProvider(ListViewModel, provideViewModel)
+  ],
+  styleUrls: [
+    'package:angular_components/app_layout/layout.scss.css',
+    'package:angular_components/css/mdc_web/card/mdc-card.scss.css',
+    'app_component.css',
+  ],
 )
-class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+class AppComponent implements OnInit, OnDestroy {
+  AppComponent(this.viewModel);
+
+  final ListViewModel viewModel;
+
+  List<Movie> movies;
+  bool loading;
+
+  @override
+  void ngOnInit() {
+    viewModel.outputs.result.listen((data) => movies = data);
+    viewModel.outputs.loading.listen((isLoading) => loading = isLoading);
+
+    viewModel.inputs.start('avenger');
+  }
+
+  @override
+  void ngOnDestroy() {
+    viewModel.dispose();
+  }
 }
